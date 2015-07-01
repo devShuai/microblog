9eff013da4282cc66389f797bb1647d1a71c2028
Shao Shuai <shshilmh@gmail.com>
2015-07-02 00:54:00 +0800

    handler 404 and 500 error

--- app/views.py
+++ app/views.py
@@ -5,6 +5,16 @@ from .forms import LoginForm, EditForm
 from .models import User
 from datetime import datetime
 
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
 @app.route('/')
 @app.route('/index')
 def index():
