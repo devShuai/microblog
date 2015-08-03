feed9a2f78f62dcb41bb4d14022d00dfc6c4796c
Shao Shuai <shshilmh@gmail.com>
2015-08-03 17:50:01 +0800

    full text search

--- config.py
+++ config.py
@@ -25,3 +25,6 @@ POSTS_PER_PAGE = 3
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+MAX_SEARCH_RESULTS = 50
